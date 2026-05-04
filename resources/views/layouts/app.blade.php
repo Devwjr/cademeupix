114366df659bdde9896a1ef349661ea35a69ad10
Devwjr <wesley.7996450@gmail.com>
2026-05-04 12:32:29 -0300

    Corrige layout: @stack(styles) no head para carregar estilos da landing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @stack('styles')
 </head>
 <body>
     @auth
@@ -99,7 +100,6 @@
         @endif
     </div>
 
-    @stack('styles')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
